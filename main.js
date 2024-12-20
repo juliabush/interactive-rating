0dca39ed6a411ef862c10ffd856e8d01ba79e42d
juliabush <j.elizabethbush@gmail.com>
2024-12-20 21:28:49 +0100

    Trying to code in active states

---
+++ main.js
@@ -0,0 +1,7 @@
+const ratingCardButtonCircles = document.querySelectorAll(".rating-card__button--circle");
+
+ratingCardButtonCircles.forEach(ratingCardButtonCircle => {
+    ratingCardButtonCircle.addEventListener("click", event => {
+        ratingCardButtonCircle.classList.toggle("active");
+    });
+});
