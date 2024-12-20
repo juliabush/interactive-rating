4def8a0501eee0f5cbfdd92c42110650e98e4ad5
juliabush <j.elizabethbush@gmail.com>
2024-12-20 22:46:19 +0100

    Trying to make buttons interactive

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const ratingCardGroupButtons = document.querySelector('.rating-card__group-buttons');
-
-const ratingCardButtonCircles =  ratingCardGroupButtons.querySelectorAll(".rating-card__button--circle");
+const ratingCardButtonCircles =  document.querySelectorAll(".rating-card__button--circle");
 
 ratingCardButtonCircles.forEach(ratingCardButtonCircle => {
     ratingCardButtonCircle.addEventListener("click", event => {
